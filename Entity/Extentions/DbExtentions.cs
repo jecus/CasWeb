a71e1f6e73369df30f22c50a24bbb4e00fad9096
Jenya Jecus <evgenii.babak@outlook.com>
2019-02-02 16:36:48 +0300

    ExtentionForDB

---
+++ Entity/Extentions/DbExtentions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity.Extentions
+{
+	public static class DbExtentions
+	{
+		public static IQueryable<T> OnlyActive<T>(this IQueryable<T> queryable) where  T : class,IBaseEntity
+		{
+			return queryable.Where(i => !i.IsDeleted);
+		}
+	}
+}
