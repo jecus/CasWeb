8be75fe1d22b21a691b2bdc45a1caf57aa873923
Jenya Jecus <evgenii.babak@outlook.com>
2019-02-06 22:43:23 +0300

    AircraftLayout

---
+++ WebDevelopment/Controllers/AircraftController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebDevelopment.Helper;
+
+namespace WebDevelopment.Controllers
+{
+
+    public class AircraftController : Controller
+    {
+        public IActionResult Index(int aircraftId)
+        {
+	        ViewData["MainMenu"] = AircraftMainMenu.Items.OrderByDescending(i => i.SubMenu.Count() > 0).ThenBy(i => i.Header).ToList();
+
+			return View();
+        }
+    }
+}
