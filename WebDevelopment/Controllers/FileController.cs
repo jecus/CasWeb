233bca950bac92b80f24d37464831e307aa92084
Jenya Jecus <evgenii.babak@outlook.com>
2019-02-18 16:43:42 +0300

    fix

--- WebDevelopment/Controllers/FileController.cs
+++ WebDevelopment/Controllers/FileController.cs
@@ -18,16 +18,11 @@ namespace WebDevelopment.Controllers
 		[HttpPost]
 		public async Task<ActionResult> OpenPdf(int? fileId)
 		{
-			//var file = await _db.Files
-			//	.AsNoTracking()
-			//	.FirstOrDefaultAsync(i => i.ItemId == fileId);
+			var file = await _db.Files
+				.AsNoTracking()
+				.FirstOrDefaultAsync(i => i.ItemId == fileId);
 
-			//return File(file.FileData, System.Net.Mime.MediaTypeNames.Application.Octet, file.FileName);
-
-			var fileName = $"2.JPG";
-			var filepath = $"D:\\Фото\\{fileName}";
-			byte[] fileBytes = System.IO.File.ReadAllBytes(filepath);
-			return File(fileBytes, "application/x-msdownload", fileName);
+			return File(file.FileData, System.Net.Mime.MediaTypeNames.Application.Octet, file.FileName);
 		}
 	}
 }
