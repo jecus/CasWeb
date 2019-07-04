8f332855b2cddb0a5fde7cb342e29fe29dee7380
Misha <Misha@DESKTOP-2KBGUT9>
2019-07-04 19:04:00 +0600

    удаление из BaseComponentView

--- BusinessLayer/Views/BaseComponentView.cs
+++ BusinessLayer/Views/BaseComponentView.cs
@@ -1,36 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using BusinessLayer.Dictionaties;
+﻿using BusinessLayer.Dictionaties;
 
 namespace BusinessLayer.Views
 {
-	public class BaseComponentView : BaseView
+	public class BaseComponentView : ComponentView
 	{
 		private BaseComponentType _baseComponentType;
-
-
-		public AircraftModelView ModelId { get; set; }
-
-		public string Manufacturer { get; set; }
-
-		public string SerialNumber { get; set; }
-
-		public string PartNumber { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayName("ManufactureDate")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
-        public DateTime? ManufactureDate { get; set; }
-
-        [DataType(DataType.Date)]
-        [DisplayName("StartDate")]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
-        public DateTime? StartDate { get; set; }
-
-		public bool IsBaseComponent { get; set; }
-
+		
 		public int BaseComponentTypeId { get; set; }
 
 		public string Position
@@ -42,13 +17,6 @@ namespace BusinessLayer.Views
 		{
 			get => BaseComponentType.GetComponentTypeById(BaseComponentTypeId);
 		}
-
-		public ComponentModelView Model { get; set; }
-
-		public List<TransferRecordView> TransferRecords { get; set; }
-
-
-
-
+		
 	}
 }
